4b3468d59940bd1675afa93b0121bbd50d592fd2
tivrama <tivrama@yahoo.com>
2016-06-08 12:31:47 -0700

    All unit tests passing.

--- test/unit/utilities.js
+++ test/unit/utilities.js
@@ -4,9 +4,39 @@ var expect = chai.expect;
 
 describe('the todo.App', function() {
   describe('the todo object', function(){
-
+    
     it('should have all the necessary methods', function(){
+      todo.App.prototype.should.have.property('render');
     });
+
+    it('expect the render to be a function', function(){
+      expect(todo.App.prototype.render).to.be.a('function');
+    });
+
+    it('assert render to a function', function(){
+      assert.typeOf(todo.App.prototype.render, 'function');
+    });
+
+    it('function should not be a number', function(){
+      todo.App.prototype.should.not.be.a('number');
+    });
+
+    it('should not equal a function', function(){
+      expect(todo.App.prototype.add).to.not.equal('function');
+    });
+
+    it('should have property add', function(){
+      todo.App.prototype.should.have.property('add');
+    });
+
+    it('expect property to have length above 1', function(){
+      expect(Object.keys(todo.App.prototype)).to.have.length.above(1);
+    });
+
+    it('should be a number', function(){
+      todo.util.getUniqueId().should.be.a('number');
+    });
+
   });
 });
 
