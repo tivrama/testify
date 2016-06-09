904c2c458f953816da808108b537e0144c3e1741
tivrama <tivrama@yahoo.com>
2016-06-08 17:40:43 -0700

    Completed Integration Test

--- test/integration/apiTests.js
+++ test/integration/apiTests.js
@@ -3,13 +3,13 @@ describe('API integration', function(){
   var server, setupStub, JSONresponse;
 
   it('todo.setup receives an array of todos when todo.init is called', function () {
+    
     // Fake JSON response
-    JSONresponse = [{ name: 'testsssssss1',  done: true}];
+    JSONresponse = { name: 'testsssssss1',  done: true, todos: [1,2,3,4,5,6,77]};
 
     // Object which makes a fake GET request
     var object = {
       method: function(json) {
-        console.log('json', json);
         // Creates a fake server
         var server = sinon.fakeServer.create();
 
@@ -17,60 +17,25 @@ describe('API integration', function(){
         server.respondWith('GET', 'http://localhost:3000/todos', [
           200,
           {'Content-Type': 'application/json'},
-          // JSON.stringify([{ name: 'SINON TESTTTT',  done: true}])
           JSON.stringify(json)
         ]);
+        
+        // Creates a stub for todo.setup
+        setupStub = sinon.stub(todo, "setup");
 
         //Invoke todo.init with false
         todo.init(false);
 
-        // STUB: then check todo.app.items -> check if an []
-        // Creates a stub for todo.setup
-        console.log('todo.setup', todo.setup);
-
-        setupStub = sinon.stub(todo, "setup");
-
-
         // Makes our fake server respond
-        server.respond('HERES A STRING');
-       
-        // server.autoRespondAfter
-        console.log('setupStub', setupStub);
+        server.respond();
+
+        // Check if res.todos is an array
+        sinon.assert.calledWith(setupStub, sinon.match.array);
        
         // Restores setup stub
         setupStub.restore();
-        
+
       }
     };
-
-    // Creates a before spy
-    var spy1 = sinon.spy(object, 'method');
-    spy1(JSONresponse);
-    sinon.assert.called(spy1);
-    // assert(spy1.withArgs([{ name: 'testsssssss1',  done: true}]));
-    // spy1.assert.calledWith([{ name: 'testsssssss1',  done: true}])
-    // Creates an after spy
-    // var spy2 = sinon.spy(object, 'method');
-
-    // sinon.assert.calledWith(spy2, [{ name: 'testsssssss2',  done: true}])
-    // assert(spy2.withArgs([{ name: 'testsssssss2',  done: true}]));
-    // assert(stub.calledBefore(spyInit));
-    // assert(stub.calledBefore(spySetup));
-
-
-
-
-    //spy.calledBefore 
-    //create setupStub to ensure to.setup doesn't create a new app
-    
-    
-
-    // spy.calledBefore(spy);
-
-    // spy.calledAfter(spy);
-
-    // Restore the spy back to its original state
-    // spy.restore();
-
   });
 });
